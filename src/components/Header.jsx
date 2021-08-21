a9c3ac88475944327026b50290cf33057587839f
Rishabh Mishra <rismis0505@gmail.com>
2021-08-21 22:38:01 +0530

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-center text-info mt-3 mb-4">CoinData</h1>
+    </div>
+  );
+};
+
+export default Header;
